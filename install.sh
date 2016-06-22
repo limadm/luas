8ef86136e25bd1d40b61a0e9c631002b5a25b200
Daniel Lima <danielm@tinyhub.tk>
2016-06-22 06:15:16 -0300

    fix: prefixed install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Installation script for luas and shell completions.
 # Published under MIT license. Daniel Lima, 2016 <danielm@tinyhub.tk>
-install -m755 -p luas "$PREFIX/usr/bin/luas"
+install -m755 -D luas "$PREFIX/usr/bin/luas"
 install -m644 -D luas-completion.bash "$PREFIX/etc/bash_completion.d/luas-completion.bash"
 install -m644 -D luas-completion.fish "$PREFIX/etc/fish/completions/luas-completion.fish"
